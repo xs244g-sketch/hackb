5bdbc8091c8630b8da0b58616df441a357358c1d
xs244g-sketch <xs244g@gmail.com>
2026-03-06 10:01:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports={
     probot_ids :'1478778390638231623',//أيدي بروبوت
-    recipientId : '',//أيدي الشخص ألي يتحول له الكريدت عندا نسخ السيرفر
-    price : '0',//سعر النسخ
-    serverid : '1478777731457089675',//أيدي سيرفارك
-    token : 'MTQ1MzI5MjEyMTYyNzk1MTI5Ng.GiYeb6.SXRKdM2ulwDUI_e4LYe9TX2V1VLVSaSaQfR9so',//توكن بوتك
-    purchaseLogChannelId: '1478777731457089678', // روم عمليات الشراء
-    logsChannelId: '1478779115812421704' // روم السجلات
+    recipientId : '1095323768194355211',//أيدي الشخص ألي يتحول له الكريدت عندا نسخ السيرفر
+    price : '1',//سعر النسخ
+    serverid : '1472331478607200371',//أيدي سيرفارك
+    token : 'MTQ3ODc3ODM5MDYzODIzMTYyMw.GSnXgf.DGDHyWPKH_Ha3fYoXICeae6L9ingwc4aSONa04',//توكن بوتك
+    purchaseLogChannelId: '1472429920880689316', // روم عمليات الشراء
+    logsChannelId: '1399278745122242612' // روم السجلات
 }
