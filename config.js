4f89ed2028ff95e953fc5044d9205477594c80c6
xs244g-sketch <xs244g@gmail.com>
2026-03-06 10:11:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports={
     recipientId : '1095323768194355211',//أيدي الشخص ألي يتحول له الكريدت عندا نسخ السيرفر
     price : '1',//سعر النسخ
     serverid : '1472331478607200371',//أيدي سيرفارك
-    token : 'MTA5NTMyMzc2ODE5NDM1NTIxMQ.GZzfVA._Tg7QTzNkjB0C9V468V_xGGdohsip5Sx42BnD8',//توكن بوتك
+    token : '.GZzfVA.',//توكن بوتك
     purchaseLogChannelId: '1472429920880689316', // روم عمليات الشراء
     logsChannelId: '1399278745122242612' // روم السجلات
 }
