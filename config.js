2924a2d8b82a21273864280169e042cfffa242e3
xs244g-sketch <xs244g@gmail.com>
2026-03-06 10:04:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports={
     recipientId : '1095323768194355211',//أيدي الشخص ألي يتحول له الكريدت عندا نسخ السيرفر
     price : '1',//سعر النسخ
     serverid : '1472331478607200371',//أيدي سيرفارك
-    token : 'MTQ3ODc3ODM5MDYzODIzMTYyMw.GSnXgf.DGDHyWPKH_Ha3fYoXICeae6L9ingwc4aSONa04',//توكن بوتك
+    token : 'MTA5NTMyMzc2ODE5NDM1NTIxMQ.GZzfVA._Tg7QTzNkjB0C9V468V_xGGdohsip5Sx42BnD8',//توكن بوتك
     purchaseLogChannelId: '1472429920880689316', // روم عمليات الشراء
     logsChannelId: '1399278745122242612' // روم السجلات
 }
